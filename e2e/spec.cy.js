78c8e4b3693ef1f191dfa9df0ec27c5e5c49f37b
joao <joao@tsigo.com.br>
2025-04-13 15:41:17 -0300

    seletores CSS

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -1,18 +1,38 @@
-describe('Teste completo de esperas e assercoes', () => {
-    it('Devo acessar a pagina, aguardar os elementos', () => {
-        
-    cy.visit('https://example.cypress.io/todo');  
-    
-   cy.get('.todo-list').sould('be.visible');
-
-   cy.get('.new-todo').type('Estudar Cypress{enter}');
-   cy.get('.todo-list li').should('contain.text', 'Estudar Cypress');
-
-   cy.get('.todo-list li').last().find('.toggle').click();
-   cy.get('.todo-list li').last().should('have.class', 'completed');
-
-   cy.get('.todo-list li').should('have.length', 3);
-
-   cy.contains('.todo-list li', 'Estudar Cypress').should('be.visible');
-});
-})
+describe('Teste da Página de Cadastro', () => {
+    beforeEach(() => {
+      cy.visit('http://127.0.0.1:5500/') 
+    })
+  
+    it('Deve exibir o formulário de cadastro', () => {
+      cy.get('#register-form').should('be.visible')
+    })
+  
+    it('Deve conter 3 inputs com a classe .input-field', () => {
+      cy.get('.input-field').should('have.length', 3)
+    })
+  
+    it('Deve verificar o texto do botão de cadastro', () => {
+      cy.get('#register-button').should('have.text', 'Cadastrar')
+    })
+  
+    it('Deve selecionar o link de login pela classe', () => {
+      cy.get('.login-link').should('be.visible')
+    })
+  
+    it('Deve preencher o campo de nome com seletor type="text"', () => {
+      cy.get('input[type="text"]').type('João da Silva')
+    })
+  
+    it('Deve preencher o campo de e-mail com seletor type="email"', () => {
+      cy.get('input[type="email"]').type('joao@email.com')
+    })
+  
+    it('Deve preencher o campo de senha com seletor type="password"', () => {
+      cy.get('input[type="password"]').type('senha123')
+    })
+  
+    it('Deve clicar no botão de cadastro com seletor CSS', () => {
+      cy.get('#register-button').click()
+    })
+  })
+  
